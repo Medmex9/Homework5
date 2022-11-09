6bb3bfe829f080b7ec08ece3417e152534dd1a27
Тимофей <timon.from25@gmail.com>
2022-11-09 19:00:54 +0700

    HW5 part.1

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,28 @@
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {{
 
         int[] numbers = new int[3];
         numbers[0] = 1;
         numbers[1] = 2;
         numbers[2] = 3;
-        for (int i =0;i<=2; i++ ) {
-            System.out.println(numbers[i]);}
+        for (int i =0;i<numbers.length; i++ ) {
+            if (numbers[1]%2!=1&&numbers[2]%2==1)
+                numbers[0]+=1;
+                numbers[2]+=1;
+            System.out.print(numbers[i]);
+            if (numbers.length>0) System.out.print(", ");
+           }}
+        System.out.println("\n");
         float [] fractional = {1.57f, 7.654f, 9.986f};
-        System.out.println(fractional[2] + ", "+fractional[1] + ", "+fractional[0]);
+        System.out.print(fractional[2] + ", "+fractional[1] + ", "+fractional[0]);
+        System.out.println("\n");
 
-        int [] row = {88,33,34,12,34,23,11};
-        System.out.println(row[6]+ ", "+row[5]+ ", "+row[4]+ ", "+row[3]
-                + ", "+row[2]+ ", "+row[1]+ ", "+row[0]);
+        int [] row = {1,2,3,4,5,6,7};
+        for (int z = row.length -1; z>=0; z--) {
+            System.out.print(row[z]);
+            if (z>0)
+                System.out.print(", ");
 
-    }}
+
+
+    }}}
